c5ce80e4caec572a5150b1621293152755869d71
João Vitor Castilho <joaovicastilho0@gmail.com>
2023-06-07 12:52:22 -0300

    Conecting to MongDB with repository and service layers

---
+++ src/main/java/com/castilho/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.castilho.workshopmongo.repository;
+
+import com.castilho.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
