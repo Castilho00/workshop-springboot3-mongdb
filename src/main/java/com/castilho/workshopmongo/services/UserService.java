07d75d42e8d08774847c20398c970ce4f23fcaaf
João Vitor Castilho <joaovicastilho0@gmail.com>
2023-06-22 15:43:01 -0300

    getting an user by id | exceptions created

--- src/main/java/com/castilho/workshopmongo/services/UserService.java
+++ src/main/java/com/castilho/workshopmongo/services/UserService.java
@@ -2,19 +2,26 @@ package com.castilho.workshopmongo.services;
 
 import com.castilho.workshopmongo.domain.User;
 import com.castilho.workshopmongo.repository.UserRepository;
+import com.castilho.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class UserService{
+public class UserService {
 
     @Autowired
     private UserRepository repository;
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
 }
