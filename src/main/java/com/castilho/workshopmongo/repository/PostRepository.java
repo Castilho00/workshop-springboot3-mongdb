db2127abe67f4f322873f0a2bc8db18c3d8f2efa
João Vitor Castilho <joaovicastilho0@gmail.com>
2023-06-27 17:33:56 -0300

    simple query using @Query annotation

--- src/main/java/com/castilho/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/castilho/workshopmongo/repository/PostRepository.java
@@ -2,10 +2,14 @@ package com.castilho.workshopmongo.repository;
 
 import com.castilho.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
